9cc376001d430a15b51aeaf5d9246e6c527eb684
Andrew Smith <andrew_smith1984@hotmail.com>
2022-04-11 23:39:24 +1000

    Add files via upload
    Database config - send to php page

---
+++ config/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "rmit.australiaeast.cloudapp.azure.com";
+$username = "dropbears";
+$password = "abc123";
+$databaseName = "dropbears";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  /* echo "Connected successfully"; */
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
